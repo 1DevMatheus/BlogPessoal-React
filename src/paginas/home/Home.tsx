fc6bed4a2245a54c9aebdba6927b00f499ca3e8f
Matheus S. Oliveira <matheuskiller6@gmail.com>
2023-02-28 17:05:31 -0300

    BlogPessoal React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import react from "react";
+import "./Home.css";
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img className="img" src="https://conteudo.imguol.com.br/c/noticias/5f/2022/07/12/12jul2022-quinto-de-stephan-grupo-de-cinco-galaxias-que-fica-na-constelacao-de-pegasus-a-cerca-de-290-milhoes-anos-luz-da-terra-imagem-foi-capturada-pelo-james-webb-1657639221855_v2_900x506.jpg" alt="" />
+        </>
+    );
+}
+
+export default Home;
