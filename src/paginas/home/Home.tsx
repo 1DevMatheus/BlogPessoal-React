61dd2392e92feb5f00fc098181fbf94118bac4ae
Matheus S. Oliveira <matheuskiller6@gmail.com>
2023-03-01 13:27:28 -0300

    Blog Pessoal

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,30 @@
 import react from "react";
+import {Button, Grid, Paper, Typography} from '@material-ui/core';
 import "./Home.css";
+import Box from '@mui/material/Box';
+import { Block } from "@material-ui/icons";
 
 function Home(){
     return (
         <>
-            <h1 className="titulo">Home</h1>
-            <img className="img" src="https://conteudo.imguol.com.br/c/noticias/5f/2022/07/12/12jul2022-quinto-de-stephan-grupo-de-cinco-galaxias-que-fica-na-constelacao-de-pegasus-a-cerca-de-290-milhoes-anos-luz-da-terra-imagem-foi-capturada-pelo-james-webb-1657639221855_v2_900x506.jpg" alt="" />
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={12} sm={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
